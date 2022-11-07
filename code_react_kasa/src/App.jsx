fc30059ddb7028e9e11c39cf2aef25ea2fb0ca4c
ptijjo <bangouraibrahima57@gmail.com>
2022-11-07 23:03:45 +0100

    Last version

--- code_react_kasa/src/App.jsx
+++ code_react_kasa/src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import Router from './Components/Router';
-import Accueil from './Pages/Accueil';
-import About from './Pages/About';
-import { Route } from "react-router-dom";
-import NotFoundPage from './Pages/NotFoundPage';
-import FicheLogement from './Pages/FicheLogement';
+
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" element={<Accueil />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/fichelogement/:id" element={<FicheLogement />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Router>
-
+    <Router />
   );
 };
 
--- code_react_kasa/src/Components/Router.jsx
+++ code_react_kasa/src/Components/Router.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
-const Router = (props) => {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Accueil from '../Pages/Accueil';
+import About from '../Pages/About';
+import NotFoundPage from '../Pages/NotFoundPage';
+import FicheLogement from '../Pages/FicheLogement';
+const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                {props.children}
+                <Route path="/" element={<Accueil />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/fichelogement/:id" element={<FicheLogement />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
 
